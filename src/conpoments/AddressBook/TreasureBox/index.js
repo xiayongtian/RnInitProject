e78893457ccba76968adddd0603832b22745e0b1
xiawei <2290545426@qq.com>
2020-04-14 13:04:32 +0800

    完成：百宝箱的点击其他按钮，最近访问存在数据

--- src/conpoments/AddressBook/TreasureBox/index.js
+++ src/conpoments/AddressBook/TreasureBox/index.js
@@ -5,7 +5,9 @@ import {
   ListView,
   StyleSheet,
   ImageBackground,
-  ScrollView
+  ScrollView,
+  TouchableOpacity,
+  AsyncStorage
 } from 'react-native';
 import baiRecentVisit from '../allJson/baiRecentVisit.json'
 import gaoxiaoList from '../allJson/gaoxiao.json'
@@ -15,11 +17,72 @@ import laboratory from "../allJson/laboratory.json"
 
 
 class TreasureBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentVisitList: [],
+      // oldList: []
+    };
+
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem(
+      'recentList', (error, result) => {
+        console.log(result)
+        this.setState({
+          recentVisitList: JSON.parse(result)
+        })
+      })
+  }
+  /**
+   * 保存点击应用到AsyncStorage，最近访问显示
+   */
+  saveRecentVisit = (item) => {
+    // AsyncStorage.removeItem('key', (error) => {
+    // })
+    AsyncStorage.getItem(
+      'recentList',
+      (error, result) => {
+        if (error) {
+        } else {
+          let tempArray = [];
+          if (result) {
+            console.log("取得Storage数值", result)
+            let oldList = JSON.parse(result)
+            // 去重
+            let temp = oldList.filter(value => {
+              return item.id !== value.id
+            })
+            temp.unshift(item)
+            this.setState({
+              recentVisitList: temp
+            })
+            tempArray = temp
+          } else {
+            tempArray.push(item)
+
+          }
+          let tempData = JSON.stringify(tempArray);
+          AsyncStorage.setItem(
+            'recentList',
+            tempData || [],
+            (error, result) => {
+              if (error) {
+                console.log('存值失败:', error);
+              } else {
+                console.log('存值成功!');
+              }
+            }
+          );
+        }
+      }
+    )
+  }
+
   render() {
     return (
-
       <View style={{ flex: 1 }}>
-        
         <View style={styles.base}>
           <ScrollView>
             <View style={{ flex: 1 }}>
@@ -50,12 +113,12 @@ class TreasureBox extends Component {
                   </View>
                 </View>
                 {/* 最近访问 */}
-                <View style={styles.recenVisit}>
+                <View style={styles.containItem}>
                   <View>
-                    <Text style={styles.recentVisitTitle}>最近访问</Text>
+                    <Text style={styles.containTitle}>最近访问</Text>
                   </View>
                   <View style={styles.item}>
-                    {baiRecentVisit && baiRecentVisit.body.map(item => {
+                    {this.state.recentVisitList && this.state.recentVisitList.map(item => {
                       return <View style={styles.content}>
                         <View>
                           <ImageBackground source={baibao[item.bgName]} imageStyle={{}} style={styles.imageStyle}>
@@ -71,62 +134,74 @@ class TreasureBox extends Component {
                 </View>
               </ImageBackground>
               {/* 高效工作 */}
-              <View style={styles.recenVisit}>
+              <View style={styles.containItem}>
                 <View>
-                  <Text style={styles.recentVisitTitle}>高效工作</Text>
+                  <Text style={styles.containTitle}>高效工作</Text>
                 </View>
+
                 <View style={styles.item}>
                   {gaoxiaoList && gaoxiaoList.body.map(item => {
-                    return <View style={styles.content}>
-                      <View>
-                        <ImageBackground source={baibao[item.bgName]} imageStyle={{}} style={styles.imageStyle}>
-                        </ImageBackground>
-                      </View>
+                    return (
+                      <TouchableOpacity onPress={() => this.saveRecentVisit(item)} style={styles.content}>
+                        {/* <View > */}
+                        <View>
+                          <ImageBackground source={baibao[item.bgName]} imageStyle={{}} style={styles.imageStyle}>
+                          </ImageBackground>
+                        </View>
 
-                      <View>
-                        <Text style={styles.itemText}>{item.bgTitle}</Text>
-                      </View>
-                    </View>
+                        <View>
+                          <Text style={styles.itemText}>{item.bgTitle}</Text>
+                        </View>
+                        {/* </View> */}
+                      </TouchableOpacity>
+                    )
+
                   })}
                 </View>
               </View>
               {/* 企业生活 */}
-              <View style={styles.recenVisit}>
+              <View style={styles.containItem}>
                 <View>
-                  <Text style={styles.recentVisitTitle}>企业生活</Text>
+                  <Text style={styles.containTitle}>企业生活</Text>
                 </View>
                 <View style={styles.item}>
                   {companyLife && companyLife.body.map(item => {
-                    return <View style={styles.content}>
-                      <View>
-                        <ImageBackground source={baibao[item.bgName]} imageStyle={{}} style={styles.imageStyle}>
-                        </ImageBackground>
-                      </View>
+                    return (
+                      <TouchableOpacity onPress={() => this.saveRecentVisit(item)} style={styles.content}>
 
-                      <View>
-                        <Text style={styles.itemText}>{item.bgTitle}</Text>
-                      </View>
-                    </View>
+                        <View>
+                          <ImageBackground source={baibao[item.bgName]} imageStyle={{}} style={styles.imageStyle}>
+                          </ImageBackground>
+                        </View>
+                        <View>
+                          <Text style={styles.itemText}>{item.bgTitle}</Text>
+                        </View>
+                      </TouchableOpacity>
+                    )
+
                   })}
                 </View>
               </View>
 
               {/* 实验室 */}
-              <View style={styles.recenVisit}>
+              <View style={styles.containItem}>
                 <View>
-                  <Text style={styles.recentVisitTitle}>实验室</Text>
+                  <Text style={styles.containTitle}>实验室</Text>
                 </View>
                 <View style={styles.item}>
                   {laboratory && laboratory.body.map(item => {
-                    return <View style={styles.content}>
-                      <View>
-                        <ImageBackground source={baibao[item.bgName]} imageStyle={{}} style={styles.imageStyle}>
-                        </ImageBackground>
-                      </View>
-                      <View>
-                        <Text style={styles.itemText}>{item.bgTitle}</Text>
-                      </View>
-                    </View>
+                    return (
+                      <TouchableOpacity onPress={() => this.saveRecentVisit(item)} style={styles.content}>
+
+                        <View>
+                          <ImageBackground source={baibao[item.bgName]} imageStyle={{}} style={styles.imageStyle}>
+                          </ImageBackground>
+                        </View>
+                        <View>
+                          <Text style={styles.itemText}>{item.bgTitle}</Text>
+                        </View>
+                      </TouchableOpacity>
+                    )
                   })}
                 </View>
               </View>
@@ -148,16 +223,16 @@ const styles = StyleSheet.create({
   tianqiContain: {
     height: 120, padding: 20, paddingTop: 10,
   },
-  recenVisit: {
+  containItem: {
     borderRadius: 20,
     padding: 5,
     margin: 15,
     paddingTop: 20,
     backgroundColor: '#fff'
   },
-  recentVisitTitle: { fontSize: 20, color: '#120f25', paddingLeft: 20, fontWeight: 'bold', paddingBottom: 5 },
+  containTitle: { fontSize: 20, color: '#120f25', paddingLeft: 20, fontWeight: 'bold', paddingBottom: 5 },
   item: { paddingBottom: 10, alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap', borderBottomLeftRadius: 20, borderBottomRightRadius: 20 },
-  content: { width: '25%', height: 100, justifyContent: 'center', alignItems: 'center', },
+  content: {  width: '25%', height: 100, justifyContent: 'center', alignItems: 'center', },
   itemText: { fontSize: 12, color: '#120f25', marginTop: 10 },
   imageStyle: { width: 40, height: 40 },
   baibaoTitle: {
